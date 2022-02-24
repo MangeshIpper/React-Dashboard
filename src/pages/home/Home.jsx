989fd539e9adffecb50d08dc13968663491cf9c3
MangeshIpperNCI <mangesh.ipper@gmail.com>
2022-02-24 00:48:34 +0000

    Added sidebar, features content.

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Chart from '../../components/chart/Chart';
+import FeaturedInfo from '../../components/featuredInfo/FeaturedInfo';
+import "./home.css";
+
+function Home() {
+  return (
+   <div className="home">
+       <FeaturedInfo />
+       <Chart />
+   </div>
+  )
+}
+
+export default Home
